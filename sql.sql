1f3471afb6b07b1661fa589b0bbe01554d4b1250
ingjesus061483 <51796225+ingjesus061483@users.noreply.github.com>
2024-09-28 22:01:51 -0500

    base de dato

--- sql.sql
+++ sql.sql
@@ -16,6 +16,39 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`caidapresion` /*!40100 DEFAULT CHARACTE
 
 USE `caidapresion`;
 
+/*Table structure for table `concentracion` */
+
+DROP TABLE IF EXISTS `concentracion`;
+
+CREATE TABLE `concentracion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `espumante` */
+
+DROP TABLE IF EXISTS `espumante`;
+
+CREATE TABLE `espumante` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `espumante_concentracion` */
+
+DROP TABLE IF EXISTS `espumante_concentracion`;
+
+CREATE TABLE `espumante_concentracion` (
+  `espumante_id` int(11) NOT NULL,
+  `concentracion_id` int(11) NOT NULL,
+  KEY `centracion_id` (`concentracion_id`),
+  KEY `espumante_concentracion_ibfk_1` (`espumante_id`),
+  CONSTRAINT `espumante_concentracion_ibfk_1` FOREIGN KEY (`espumante_id`) REFERENCES `espumante` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `espumante_concentracion_ibfk_2` FOREIGN KEY (`concentracion_id`) REFERENCES `concentracion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 /*Table structure for table `resultados` */
 
 DROP TABLE IF EXISTS `resultados`;
@@ -30,8 +63,14 @@ CREATE TABLE `resultados` (
   `reb` decimal(10,2) NOT NULL,
   `usg` decimal(10,2) NOT NULL,
   `jg` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `concentracion_id` int(11) NOT NULL,
+  `espumante_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `resultados_ibfk_1` (`concentracion_id`),
+  KEY `espumante_id` (`espumante_id`),
+  CONSTRAINT `resultados_ibfk_1` FOREIGN KEY (`concentracion_id`) REFERENCES `concentracion` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `resultados_ibfk_2` FOREIGN KEY (`espumante_id`) REFERENCES `espumante` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /* Procedure structure for procedure `Insertar` */
 
@@ -47,11 +86,34 @@ DELIMITER $$
 					IN _ub DECIMAL(10,2),
 					IN _reb DECIMAL(10,2),
 					IN _usg DECIMAL(10,2),
-					IN _jg DECIMAL(10,2))
+					IN _jg DECIMAL(10,2),
+					in _concentracion_id INT(11),
+					IN _espumante_id int (11) )
 BEGIN
-		INSERT INTO `resultados`(`deltap`,`jsl`,`holdup`,`db`,`ub`,`reb`,`usg`,`jg`)VALUES(_deltap,_jsl,_holdup,_db,_ub,_reb,_usg,_jg);
+		INSERT INTO `resultados`(`deltap`,`jsl`,`holdup`,`db`,`ub`,`reb`,`usg`,`jg`,`concentracion_id`,`espumante_id`)VALUES(_deltap,_jsl,_holdup,_db,_ub,_reb,_usg,_jg,_concentracion_id,_espumante_id);
 	END */$$
 DELIMITER ;
+
+/*Table structure for table `view_espumante_concentracion` */
+
+DROP TABLE IF EXISTS `view_espumante_concentracion`;
+
+/*!50001 DROP VIEW IF EXISTS `view_espumante_concentracion` */;
+/*!50001 DROP TABLE IF EXISTS `view_espumante_concentracion` */;
+
+/*!50001 CREATE TABLE  `view_espumante_concentracion`(
+ `espumante_id` int(11) ,
+ `espumante` varchar(10) ,
+ `concentracion_id` int(11) ,
+ `concentracion` varchar(10) 
+)*/;
+
+/*View structure for view view_espumante_concentracion */
+
+/*!50001 DROP TABLE IF EXISTS `view_espumante_concentracion` */;
+/*!50001 DROP VIEW IF EXISTS `view_espumante_concentracion` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_espumante_concentracion` AS select `esp`.`id` AS `espumante_id`,`esp`.`nombre` AS `espumante`,`conc`.`id` AS `concentracion_id`,`conc`.`nombre` AS `concentracion` from ((`espumante` `esp` join `espumante_concentracion` `pivote` on(`esp`.`id` = `pivote`.`espumante_id`)) join `concentracion` `conc` on(`conc`.`id` = `pivote`.`concentracion_id`)) */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
